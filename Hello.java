007dba9036af5b833169a4a51a74a93d7326de20
Tanjir-Mahabub <md.piyash@gmail.com>
2018-09-13 21:20:02 +0600

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Vs code is Awesome...");
+
+        greetings("Hello Tanjir")
+    }
+    public static void greetings(String name) {
+            System.out.println("Welcome "+ name);
     }
 }

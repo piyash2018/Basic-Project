9e4f1f671dcd7393a5c487cb3b2e673c405a8afd
Tanjir-Mahabub <md.piyash@gmail.com>
2018-09-13 22:14:06 +0600

    all method removed

--- Hello.java
+++ Hello.java
@@ -1,10 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Vs code is Awesome...");
-
-        greetings("Hello Tanjir")
     }
-    public static void greetings(String name) {
-            System.out.println("Welcome "+ name);
+    public static void message() {
+            System.out.println("Hoorray!! Now we are able to contribute on open source...");
     }
 }
